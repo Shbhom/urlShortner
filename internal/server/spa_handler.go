6aa514aabaa89a01b813fcbdcf5aaf2e3bd4068a
shubhom srivastava <shubhoms@hornettechnology.in>
2026-06-16 02:20:12 +0530

    feat: add spa fallback handler

---
+++ internal/server/spa_handler.go
@@ -0,0 +1,42 @@
+package server
+
+import (
+	"io/fs"
+	"net/http"
+	"path"
+	"strings"
+)
+
+type spaHandler struct {
+	staticFS  fs.FS
+	indexPath string
+	fsHandler http.Handler
+}
+
+func newSPAHandler(staticFS fs.FS, indexPath string) spaHandler {
+	return spaHandler{
+		staticFS:  staticFS,
+		indexPath: indexPath,
+		fsHandler: http.FileServer(http.FS(staticFS)),
+	}
+}
+
+func (h spaHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	p := strings.TrimPrefix(path.Clean(r.URL.Path), "/")
+	if p == "" {
+		p = "."
+	}
+
+	f, err := h.staticFS.Open(p)
+	if err == nil {
+		_, statErr := f.Stat()
+		f.Close()
+		if statErr == nil {
+			h.fsHandler.ServeHTTP(w, r)
+			return
+		}
+	}
+
+	r.URL.Path = "/" + h.indexPath
+	h.fsHandler.ServeHTTP(w, r)
+}
